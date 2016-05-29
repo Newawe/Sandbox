89a0b033492ecbbce4bf30cbfe223a8dc0f73b54
Ron Friedman <ron852719@icloud.com>
2016-05-29 12:36:41 -0700

    Create ruby.php

---
+++ ruby.php
@@ -0,0 +1,24 @@
+<?php
+    $sUrl = 'http://rextester.com/rundotnet/Run';
+    $params = array('http' => array(
+        'method' => 'POST',
+        'content' => 'Program='.$_GET["code"].'&LanguageChoiceWrapper=12'
+    ));
+    $ctx = stream_context_create($params);
+    $fp = @fopen($sUrl, 'rb', false, $ctx);
+    if (!$fp)
+    {
+        throw new Exception("Problem with $sUrl, $php_errormsg");
+    }
+    $response = @stream_get_contents($fp);
+    if ($response === false)
+    {
+        throw new Exception("Problem reading data from $sUrl, $php_errormsg");
+    }
+$obj = json_decode($response);
+if(!$obj->{'Errors'}) {
+    echo $obj->{'Result'};
+}else{
+    echo $obj->{'Errors'};
+}
+?>
